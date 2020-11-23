63a14baf5cbbfb13bbc71c11c15e907f8a1ccac3
GA-02 <68775681+GA-02@users.noreply.github.com>
2020-11-23 16:34:03 +0300

    Add files via upload

---
+++ build/precache-manifest.b66f329e906aa8b2905088ffbb973cf7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448db815a3e67299d1a47daa097eb986",
+    "url": "/GA-02/Grechka.git/index.html"
+  },
+  {
+    "revision": "90ce5fef90930d0d3feb",
+    "url": "/GA-02/Grechka.git/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "8b566cbda6c5b2efd2f6",
+    "url": "/GA-02/Grechka.git/static/css/main.ae880ad0.chunk.css"
+  },
+  {
+    "revision": "90ce5fef90930d0d3feb",
+    "url": "/GA-02/Grechka.git/static/js/2.54e36cb5.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/GA-02/Grechka.git/static/js/2.54e36cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b566cbda6c5b2efd2f6",
+    "url": "/GA-02/Grechka.git/static/js/main.8883f8a9.chunk.js"
+  },
+  {
+    "revision": "07d777c642a2e98d4e60",
+    "url": "/GA-02/Grechka.git/static/js/runtime-main.a29b11ef.js"
+  },
+  {
+    "revision": "b528eaf852da05eb9fbb826b13d854d5",
+    "url": "/GA-02/Grechka.git/static/media/roll.b528eaf8.svg"
+  }
+]);
