6f74c02b476dda42bce16f46665c9739a8e12b7b
GA-02 <68775681+GA-02@users.noreply.github.com>
2020-11-23 16:35:57 +0300

    Updates

---
+++ precache-manifest.519e5ca03bd4e1b83972abfa40bd137f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f0d7fe35d322a0a6a9998f54abc45a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90ce5fef90930d0d3feb",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "8b566cbda6c5b2efd2f6",
+    "url": "./static/css/main.ae880ad0.chunk.css"
+  },
+  {
+    "revision": "90ce5fef90930d0d3feb",
+    "url": "./static/js/2.54e36cb5.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.54e36cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b566cbda6c5b2efd2f6",
+    "url": "./static/js/main.8883f8a9.chunk.js"
+  },
+  {
+    "revision": "4e6ddaa2f078aa8f4f73",
+    "url": "./static/js/runtime-main.c44f15e4.js"
+  },
+  {
+    "revision": "b528eaf852da05eb9fbb826b13d854d5",
+    "url": "./static/media/roll.b528eaf8.svg"
+  }
+]);
